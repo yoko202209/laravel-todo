7e8120ae70b1cb6b6662af85b8075e62bdd7a4fa
yoko200209 <thatson.create@gmail.com>
2023-03-22 17:12:42 +0900

    認証機能の追加

---
+++ laravel-mytodo/resources/views/cards/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODOカード一覧</title>
+  </head>
+  <body>
+    <header>
+      <a href="#">todo</a>
+    </header>
+    <main>
+      <article>
+        <div>
+          カード一覧
+        </div>
+        <div>
+          <!-- ここにカードを読み込む -->
+        </div>
+      </article>
+    </main>
+  </body>
+</html>
