55cf30c029a654b4a13c4983322de10cbb7055a9
yoko200209 <thatson.create@gmail.com>
2023-03-25 21:49:15 +0900

    update:TeamControllerの内部処理の作成

--- laravel-mytodo/app/Http/Controllers/TeamController.php
+++ laravel-mytodo/app/Http/Controllers/TeamController.php
@@ -2,9 +2,100 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Team;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TeamController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $teams = Team::all();
+        return view('teams.index',compact('teams'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('teams.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {        
+        $team = new team();
+        $team->name = $request->input('name');
+        $team->description = $request->input('description');
+        $team->manager_user_id = Auth::id();
+        $team->save();
+
+        return redirect()->route('teams.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Team $team)
+    {
+        return view('teams.show',compact('team'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Team $team)
+    {
+        return view('teams.edit',compact('team'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Team $team)
+    {
+        $team->name = $request->input('name');
+        $team->description = $request->input('description');
+        $team->manager_user_id = Auth::id();
+        $team->save();
+
+        return redirect()->route('teams.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Team $team)
+    {
+        $team->delete();
+        return redirect()->route('teams.index');
+    }
 }
